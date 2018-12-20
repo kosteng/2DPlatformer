4c4e7adcc208083b184f77e7e11055d43cc6b48c
kosteng <2267672@mail.ru>
2018-12-20 20:46:27 +0300

    1

---
+++ Assets/Scripts/Apteka.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apteka : MonoBehaviour {
+    [SerializeField]
+    private int _health;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.gameObject.GetComponent<HeroController>().Health(_health);
+        }
+        Destroy(gameObject);
+    }
+}
