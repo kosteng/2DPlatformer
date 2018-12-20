deb57751259636f02a36f28e4c3f2494804d6b6a
kosteng <2267672@mail.ru>
2018-12-20 22:50:53 +0300

    Заливка

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -25,7 +25,7 @@ public class Bullet : MonoBehaviour {
     /// Урон который наносит пуля
     /// </summary>
     [SerializeField]
-    private float _bulletDamage;
+    private int _bulletDamage;
       
     /// <summary>
     /// Массив звуков
@@ -37,39 +37,46 @@ public class Bullet : MonoBehaviour {
     /// Ссылка на источник звука
     /// </summary>
     private AudioSource _audiosource;
-    
-    void Start () { 
+
+    /// <summary>
+    /// Ссылка на компонент RidigBody
+    /// </summary>
+    private Rigidbody2D rb;
+    void Start ()
+    { 
         // Кешируем компонент SpriteRenderer пули
         sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
         //Проверка параметра жизни пули, если параметр не равен нулю запускаем уничтожением по значению параметра
         if (_lifeTime != 0)
             Destroy(gameObject, _lifeTime);
         // получаю доступ к источнику звуков и сами звуки
         _audiosource = GameObject.FindGameObjectWithTag("AudioSource").GetComponent<AudioSource>();
-       
-      
     }
 
-
-    void Update () {
+    private void FixedUpdate()
+    {
         // запускаем пулю в направлении относительно поворота игрока
         if (!sr.flipX)
-        transform.Translate(Time.deltaTime * _speedBullet, 0, 0);
+        
+              transform.Translate(Time.deltaTime * _speedBullet, 0, 0);
+           // rb.AddForce(Vector2.right * _speedBullet, ForceMode2D.Impulse);
         if (sr.flipX)
             transform.Translate(-Time.deltaTime * _speedBullet, 0, 0);
-		
-	}
+      //  rb.AddForce(Vector2.left * _speedBullet, ForceMode2D.Impulse);
+    }
+  
      void OnCollisionEnter2D(Collision2D collision)
     {
         // если враг то убиваю пулю, отнимаю здоровье врага и проигрываю звук
         if (collision.gameObject.tag == "Enemy")
         {
             
-            collision.gameObject.GetComponent<EnemyController>().Hurt(_bulletDamage);
+           if (collision.gameObject.GetComponent<EnemyAI>()) collision.gameObject.GetComponent<EnemyAI>().Hurt(_bulletDamage);
+           if (collision.gameObject.GetComponent<BallistaController>()) collision.gameObject.GetComponent<BallistaController>().Hurt(_bulletDamage);
             _audiosource.clip = _clips[1];
-            _audiosource.Play();
-            Destroy(gameObject);
+            _audiosource.Play();    
         }
-
+        Destroy(gameObject);
     }
 }
